d6fd4c24e329463fc5e9983cf4574f95a03a7374
Роман Сопов <sopov.rv@yarbank.ru>
2015-08-21 16:38:18 +0300

    ~

--- app/rutoken/binding.cc
+++ app/rutoken/binding.cc
@@ -5,7 +5,19 @@
 
 using namespace v8;
 
+Handle<Object> OBJ;
+
 HMODULE hModule = NULL_PTR; // Хэндл загруженной библиотеки PKCS#11
+CK_SESSION_HANDLE hSession = NULL_PTR;                 // Хэндл открытой сессии
+
+CK_FUNCTION_LIST_PTR pFunctionList = NULL_PTR;         // Указатель на список функций PKCS#11, хранящийся в структуре CK_FUNCTION_LIST
+CK_C_GetFunctionList pfGetFunctionList = NULL_PTR;     // Указатель на функцию C_GetFunctionList
+
+CK_SLOT_ID_PTR aSlots = NULL_PTR;                      // Указатель на массив идентификаторов всех доступных слотов
+CK_ULONG ulSlotCount = 0;                              // Количество идентификаторов всех доступных слотов в массиве
+
+CK_RV rv = CKR_OK;                                     // Вспомогательная переменная для хранения кода возврата
+CK_RV rvTemp = CKR_OK;                                 // Вспомогательная переменная для хранения кода возврата
 
 static void LogCallback(const FunctionCallbackInfo<Value>& args) {
     if (args.Length() < 1) return;
@@ -29,27 +41,16 @@ void isModule(const FunctionCallbackInfo<Value>& args) {
     if(hModule != NULL_PTR) {
         ret = true;
     }
-    args.GetReturnValue().Set(ret);
+    args.GetReturnValue().Set(OBJ);
 }
 
 //
-// Метод инициализации
+// Метод инициализации модуля
 //
 void init(Handle<Object> target) {
 
     Isolate* isolate = Isolate::GetCurrent();
     HandleScope scope(isolate);
-
-	CK_SESSION_HANDLE hSession = NULL_PTR;                 // Хэндл открытой сессии
-
-	CK_FUNCTION_LIST_PTR pFunctionList = NULL_PTR;         // Указатель на список функций PKCS#11, хранящийся в структуре CK_FUNCTION_LIST
-	CK_C_GetFunctionList pfGetFunctionList = NULL_PTR;     // Указатель на функцию C_GetFunctionList
-
-	CK_SLOT_ID_PTR aSlots = NULL_PTR;                      // Указатель на массив идентификаторов всех доступных слотов
-	CK_ULONG ulSlotCount = 0;                              // Количество идентификаторов всех доступных слотов в массиве
-
-	CK_RV rv = CKR_OK;                                     // Вспомогательная переменная для хранения кода возврата
-	CK_RV rvTemp = CKR_OK;                                 // Вспомогательная переменная для хранения кода возврата
 
     hModule = LoadLibrary(PKCS11_LIBRARY_NAME);
 
@@ -64,6 +65,8 @@ void init(Handle<Object> target) {
     // Регистрация методов
     NODE_SET_METHOD(target, "hello",    Method);
     NODE_SET_METHOD(target, "isModule", isModule);
+
+    OBJ = target;
 }
 
 //
